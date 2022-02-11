336eadf3cd852a008a7b3ad5b2cd189850d0c5a6
rahulsbharadwaj <43632774+rahulsbharadwaj@users.noreply.github.com>
2022-02-11 22:47:26 +0530

    made it better

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,30 +84,13 @@
 
 }(jQuery));
 
-const listItems = document.querySelectorAll('.main li');
-const allimages = document.querySelectorAll('.main .container-fluid .images');
- 
-function toggleActiveClass(active){
-    listItems.forEach(item => {
-      item.classList.remove('active');
-    })
-    active.classList.add('active');
-}
- 
-function toggleimages(dataClass){
-    if(dataClass === 'all'){
-        for(let i = 0; i<allimages.length; i++){
-            allimages[i].style.display = 'block';
-        }
-    }else{
-        for(let i = 0; i<allimages.length; i++)
-            allimages[i].dataset.class === dataClass ? allimages[i].style.display = 'block' : allimages[i].style.display = 'none';
-    }
-}
- 
-for(let i = 0; i < listItems.length; i++){
-    listItems[i].addEventListener('click', function(){
-        toggleActiveClass(listItems[i]);
-        toggleimages(listItems[i].dataset.class);
-    });
-}
+$(function(){
+  $(".filtering").on("click", "span", function () {
+      var a = $(".gallery").isotope({});
+      var e = $(this).attr("data-filter");
+      a.isotope({ filter: e });
+  });
+  $(".filtering").on("click", "span", function () {
+      $(this).addClass("active").siblings().removeClass("active");
+  });
+})
